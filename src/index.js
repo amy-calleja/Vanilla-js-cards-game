7c93cdbf4304f928cca6795d9f8c60584ecc0f76
AmyCalleja <amy.calleja@live.com>
2022-04-28 17:09:49 +0200

    added matching cards effect

--- src/index.js
+++ src/index.js
@@ -52,8 +52,9 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector('#grid')
-const cardsChosen = []
-
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
 
 function createBoard () {
     for (let i = 0; i < cardArray.length; i++) {
@@ -63,16 +64,36 @@ function createBoard () {
        card.addEventListener('click', flipCard)
     gridDisplay.appendChild(card)
 
-    }
+    }                               
 }
 
 createBoard()
+
+function checkMatch() {
+const cards = document.querySelectorAll('#grid img')
+console.log(cards)
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('You found a match!')
+      cards[cardsChosenIds[0]].setAttribute('src', 'src/images/white.png')
+      cards[cardsChosenIds[1]].setAttribute('src', 'src/images/white.png')
+        
+      cards[cardsChosenIds[0]].removeEventListener('click', 'flipCard')
+        cards[cardsChosenIds[0]].removeEventListener('click', 'flipCard')
+   
+        cardsWon.push(cardsChosen)
+    }
+
+    cardsChosen = []
+}
 
 function flipCard() {
     let cardId = this.getAttribute('data-id')
    
     cardsChosen.push(cardArray[cardId].name)
-    console.log('clicked', cardId)
-    console.log(cardsChosen)
+  cardsChosenIds.push(cardId)
+
     this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout( checkMatch, 500)
+    }
 }
