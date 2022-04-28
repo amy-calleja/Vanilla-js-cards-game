91d3b5a488f8a8317e2ef53f4be94c0378e333a4
AmyCalleja <amy.calleja@live.com>
2022-04-28 15:44:14 +0200

    Created new repository, imported docs

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import "./styles.css";
+
+const cardArray = [
+  {
+    name: "cheeseburger",
+    img: "images/cheeseburger.png"
+  },
+  {
+    name: "fries",
+    img: "images/fries.png"
+  },
+  {
+    name: "hotdog",
+    img: "images/hotdog.png"
+  },
+  {
+    name: "ice-cream",
+    img: "images/ice-cream.png"
+  },
+  {
+    name: "milkshake",
+    img: "images/milkshake.png"
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.png"
+  },
+  {
+    name: "cheeseburger",
+    img: "images/cheeseburger.png"
+  },
+  {
+    name: "fries",
+    img: "images/fries.png"
+  },
+  {
+    name: "hotdog",
+    img: "images/hotdog.png"
+  },
+  {
+    name: "ice-cream",
+    img: "images/ice-cream.png"
+  },
+  {
+    name: "milkshake",
+    img: "images/milkshake.png"
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.png"
+  }
+];
+
+cardArray.sort(() => 0.5 - Math.random());
+
+console.log(cardArray);
